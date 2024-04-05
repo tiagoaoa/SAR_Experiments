775dc15a12322bc6f1381ffafb4ec0cde5485a65
Tiago Alves <tiagoaoa@gmail.com>
2024-04-05 02:17:11 -0300

    Add files via upload

--- bounding_boxes.py
+++ bounding_boxes.py
@@ -12,12 +12,14 @@ cos = np.cos
 sin = np.sin
 
 def GetGeoCoordinates(gt, x, y):
-    """ receives a GeoTransform and a pair of pixel coordinates
-        returns a pair of corresponding latitude and longitude"""
-    xm, xs, xr, ym, ys, yr = gt
-    x_rotated = np.array(cos(xr), sin(xr))
-    y_rotated = np.array(-sin(yr), cos(yr))
-    x, y = x_rotated + y_rotated
+    """receives a GeoTransform and a pair of pixel coordinates
+    returns a pair of corresponding latitude and longitude"""
+    xm, xs, xr, ym, yr, ys = gt
+    """Comented out the rotation lines, because our GeoTiff's will probably be all North-up (0 rotation)""" 
+    #x_rotated = x*np.array((cos(xr), sin(xr)))
+    #y_rotated = y*np.array((-sin(yr), cos(yr)))
+    #print("Rotated2 {}".format(x_rotated + y_rotated))
+    #x, y = x_rotated + y_rotated
     return (x*xs + xm, y*ys + ym)
 
 
@@ -27,6 +29,7 @@ fname = sys.argv[1]
 gtif = gdal.Open(fname)
 gtransform = gtif.GetGeoTransform()
 
+print("GT {}".format(gtransform))
 gtif.Close()
 
 
@@ -37,7 +40,7 @@ gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
 
 #This code converts the grayscale image to a binary one (each pixel has just one bit, black or white) in order to apply simple computer vision algorithms to detect objects
-thresh = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY)[1]
+thresh = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY)[1]
 
 
 cv2.imwrite('binary.png', thresh) #saving the binary image just for debugging purposes, this step can be commented out
@@ -46,13 +49,14 @@ cv2.imwrite('binary.png', thresh) #saving the binary image just for debugging pu
 result = img.copy()
 contours = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 contours = contours[0] if len(contours) == 2 else contours[1]
-stride = 5
+stride = 100
 for cntr in contours:
     x,y,w,h = cv2.boundingRect(cntr)
-    w,h = w+stride, h+stride
+    #w,h = w+stride, h+stride
     cv2.rectangle(result, (x, y), (x+w, y+h), (0, 0, 255), 2)
-    #print("x,y,w,h:",x,y,w,h)
-    print(GetGeoCoordinates(gtransform, x, y))
+    ul_x, ul_y = GetGeoCoordinates(gtransform, x-stride, y-stride)
+    dw_x, dw_y = GetGeoCoordinates(gtransform, x+stride, y+stride)
  
+    print("{} {} {} {}".format(ul_x, ul_y, dw_x, dw_y))
 cv2.imwrite('bounding.png',result)      
 
